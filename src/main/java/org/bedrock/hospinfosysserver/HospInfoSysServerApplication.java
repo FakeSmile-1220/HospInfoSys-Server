9b32671bd45a66c8ec0c52348e44c4cad9bbbe07
Icbitic <icbitic@foxmail.com>
2024-06-13 00:38:50 +0800

    initial commit

---
+++ src/main/java/org/bedrock/hospinfosysserver/HospInfoSysServerApplication.java
@@ -0,0 +1,13 @@
+package org.bedrock.hospinfosysserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospInfoSysServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospInfoSysServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bedrock/hospinfosysserver/HospInfoSysServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedrock.hospinfosysserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospInfoSysServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
